e4d006b3414e980c6cc6d3996911ba25ade13ada
Marc <purotx2014@gmail.com>
2019-09-02 12:48:08 -0400

    start

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+var generatedNumber = Math.floor((Math.random() * 96) + 1);
+var wins;
+var losses;
+var crystal1 = Math.floor((Math.random() * 9) + 1);
+var crystal2 = Math.floor((Math.random() * 21) + 1);
+var crystal3 = Math.floor((Math.random() * 16) + 1);
+var crystal4 = Math.floor((Math.random() * 29) + 1);
+var scoreBoard;
+
+
+
+
+document.getElementById('generatedNumber').innerHTML = generatedNumber;
+document.getElementById('wins').innerHTML = wins;
