05f39a6538f3bf0c873ea9bdc464a3c82ba552fc
Marc <purotx2014@gmail.com>
2019-09-03 09:36:20 -0400

    firstTest

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,83 @@
-var generatedNumber = Math.floor((Math.random() * 96) + 1);
-var wins;
-var losses;
+var winningNumber = Math.floor((Math.random() * 146) + 30 );
+var wins = 0;
+var losses = 0;
 var crystal1 = Math.floor((Math.random() * 9) + 1);
 var crystal2 = Math.floor((Math.random() * 21) + 1);
 var crystal3 = Math.floor((Math.random() * 16) + 1);
 var crystal4 = Math.floor((Math.random() * 29) + 1);
-var scoreBoard;
+var scoreBoard = 0;
+
+{
+
+$("#crystal1").on("click", function(){
+    scoreBoard += crystal1;
+
+    if (scoreBoard === winningNumber){
+        wins++;   
+    }
+
+    if (scoreBoard > winningNumber){
+        losses++;  
+    }
+    document.getElementById('scoreBoard').innerHTML = scoreBoard;
+    document.getElementById('wins').innerHTML = ('Win = ' + wins);
+    document.getElementById('losses').innerHTML = ('Losses = ' + losses);
+});
+$('#crystal2').on('click', function(){ 
+    scoreBoard += crystal2;
+
+    if (scoreBoard === winningNumber){
+        wins++;   
+    }
+
+    if (scoreBoard > winningNumber){
+        losses++;  
+    }
+    document.getElementById('scoreBoard').innerHTML = scoreBoard;
+    document.getElementById('wins').innerHTML = ('Win = ' + wins);
+    document.getElementById('losses').innerHTML = ('Losses = ' + losses);
+});
+
+$('#crystal3').on('click', function(){ 
+    scoreBoard += crystal3;
+    
+    if (scoreBoard === winningNumber){
+        wins++;   
+    }
+
+    if (scoreBoard > winningNumber){
+        losses++;  
+    }
+    document.getElementById('scoreBoard').innerHTML = scoreBoard;
+    document.getElementById('wins').innerHTML = ('Win = ' + wins);
+    document.getElementById('losses').innerHTML = ('Losses = ' + losses);
+});
+
+$('#crystal4').on('click', function(){ 
+    scoreBoard += crystal4;
+
+    if (scoreBoard === winningNumber){
+        wins++;   
+    }
+
+    if (scoreBoard > winningNumber){
+        losses++;  
+    }
+    document.getElementById('scoreBoard').innerHTML = scoreBoard;
+    document.getElementById('wins').innerHTML = ('Win = ' + wins);
+    document.getElementById('losses').innerHTML = ('Losses = ' + losses);
+});
 
 
+   
+document.getElementById('wins').innerHTML = ('Win = ' + wins);
+document.getElementById('losses').innerHTML = ('Losses = ' + losses);
+document.getElementById('scoreBoard').innerHTML = scoreBoard;
+document.getElementById('winningNumber').innerHTML = winningNumber;
 
+}
 
-document.getElementById('generatedNumber').innerHTML = generatedNumber;
-document.getElementById('wins').innerHTML = wins;
+console.log(crystal1);
+console.log(crystal2);
+console.log(crystal3);
+console.log(crystal4); 
